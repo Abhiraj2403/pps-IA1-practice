725d80b50b7d0a4c78a0387bf286753d029cc127
Abhiraj Rai <94587983+Abhiraj2403@users.noreply.github.com>
2022-02-01 06:34:04 +0000

    p3original.c

---
+++ p3original.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int input()
+{
+  int n;
+  printf("enter the value of n \n");
+  scanf("%d",&n);
+  return n;
+}
+int aray(int n)
+{
+  int i, sum;
+  sum=0;
+  for(i=0;i<n;++i)
+  {
+    sum=sum+i;
+  }
+  return sum;
+}
+void output(int n,int result)
+{
+  printf("sum of first %d no. is %d",n,result);
+}
+int main()
+{
+  int n,sum;
+  n=input();
+  sum=aray(n);
+  output(n,sum);
+  return 0;
+}
